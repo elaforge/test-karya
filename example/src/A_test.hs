cc7ea49490a3baf58ebc7c3986937c15ab65d027
Evan Laforge <qdunkan@gmail.com>
2018-06-07 19:23:31 -0700

    ported from karya: make Process.conversation join on stdout and stderr threads
    Otherwise it's nondeterministic whether Exit comes before or after the last
    lines of output.

--- example/src/A_test.hs
+++ example/src/A_test.hs
@@ -6,3 +6,4 @@ import qualified A
 test_f = do
     let f = A.f
     equal (f 4) 8
+    equal (f 4) 9
--- example/src/RunTests.hs
+++ example/src/RunTests.hs
@@ -1,5 +1,7 @@
 {-# OPTIONS_GHC -F -pgmF test-karya-generate
     -optF --jobs=4
+    -optF --clear-dir
     -optF --output=dist/test-output
+    -optF --check-output
     -optF .
 #-}
