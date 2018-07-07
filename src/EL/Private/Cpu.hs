1f079700dc5da62f4b4c1f9d40285c2499a03946
Evan Laforge <qdunkan@gmail.com>
2018-07-07 12:41:44 -0700

    make CPUS env var override Cpu.physicalCores
    Ported from karya.

--- src/EL/Private/Cpu.hs
+++ src/EL/Private/Cpu.hs
@@ -1,24 +1,37 @@
 {-# LANGUAGE OverloadedStrings #-}
+-- | Get info about CPUs.
 module EL.Private.Cpu where
 import qualified Data.Set as Set
 import qualified Data.Text as Text
 import Data.Text (Text)
 import qualified Data.Text.IO as Text.IO
 
+import qualified System.Environment as Environment
 import qualified System.Info
 import qualified System.Process as Process
 
+import qualified Text.Read as Read
 
+
+-- | Get number of physical cores.  This can be overidden with a CPUS
+-- environment variable.  This is useful if you are running on a VM in CI
+-- and don't agree with how many cores it claims to have.
 physicalCores :: IO Int
-physicalCores = case System.Info.os of
+physicalCores = maybe getPhysicalCores return =<< envCores
+
+getPhysicalCores :: IO Int
+getPhysicalCores = case System.Info.os of
     "darwin" -> read <$>
         Process.readProcess "/usr/sbin/sysctl" ["-n", "hw.physicalcpu"] ""
-    "linux" -> cpuinfoPhysical <$> Text.IO.readFile "/proc/cpuinfo"
+    "linux" -> linuxPhysicalCores <$> Text.IO.readFile "/proc/cpuinfo"
     _ -> error $ "unknown platform: " ++ System.Info.os
 
--- | Parse /proce/cpuinfo for physical cpu count.
-cpuinfoPhysical :: Text -> Int
-cpuinfoPhysical = length . unique . map cpu . Text.splitOn "\n\n"
+envCores :: IO (Maybe Int)
+envCores = (Read.readMaybe =<<) <$> Environment.lookupEnv "CPUS"
+
+-- | Parse /proc/cpuinfo for physical cpu count.
+linuxPhysicalCores :: Text -> Int
+linuxPhysicalCores = length . unique . map cpu . Text.splitOn "\n\n"
     where
     -- unique pairs of (physical id, core id)
     cpu = filter (\s -> any (`Text.isPrefixOf` s) ["physical id", "core id"])
@@ -26,15 +39,3 @@ cpuinfoPhysical = length . unique . map cpu . Text.splitOn "\n\n"
 
 unique :: Ord a => [a] -> [a]
 unique = Set.toList . Set.fromList
-
--- if [[ -r /proc/cpuinfo ]]; then
---     # Count unique (physical id, core id) pairs.
---     # Wow linux, seriously?
---     exec egrep 'core id|physical id' /proc/cpuinfo | tr -d '\n' \
---         | sed s/physical/\\nphysical/g | grep -v '^$' | sort | uniq | wc -l
--- elif [[ -x /usr/sbin/sysctl ]]; then # OS X
---     exec /usr/sbin/sysctl -n hw.physicalcpu
--- else
---     echo 1
--- fi
-
